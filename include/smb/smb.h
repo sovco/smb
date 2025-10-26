4c59d4ff02165e80f503916bba29aba571d8b227
sovco <no@email.stop>
2025-10-26 09:22:17 +0100

    somewhat usable now

--- include/smb/smb.h
+++ include/smb/smb.h
@@ -3,14 +3,6 @@
 
 #include <time.h>
 
-static inline long smb_timestamp_ms();
-static inline long smb_timespec_to_timestamp_ms(struct timespec *time);
-static inline unsigned long smb_timespec_to_timestamp_ns(struct timespec *time);
-static inline void smb_abs_timeout(struct timespec *restrict time, long offset_ms);
-static inline struct timespec smb_time_now();
-
-static const int SMB_DEFAULT_RUNS_COUNT = 1000;
-
 typedef struct
 {
     struct timespec *timestamps;
@@ -25,8 +17,27 @@ typedef struct
     size_t total_runs;
 } smb_benchmark_options;
 
-// static inline void smb_print_result(const smb_benchmark_result *restrict result);
-static inline unsigned long smb_average_duration_calc_ns(const smb_benchmark_result *result);
+typedef struct
+{
+    size_t runs_count;
+    double average;
+    double std_dev;
+    double median;
+    double min;
+    double max;
+} smb_benchmark_report;
+
+static const int SMB_DEFAULT_RUNS_COUNT = 1000;
+
+static inline unsigned long smb_timespec_to_timestamp_ns(struct timespec *time);
+
+static inline void smb_print_report(const smb_benchmark_report *restrict report);
+static inline double smb_average_duration_calc_ns(const double *durations, const size_t count);
+static inline void smb_durations_calc_ns(const smb_benchmark_result *result, double *ret);
+static inline double smb_std_dev_calc_ns(const double *durations, const double mean, const size_t count);
+static inline void smb_sort_durations(double *durations, const size_t count);
+static inline double smb_determine_median_ns(const double *sorted_durations, const size_t count);
+static inline smb_benchmark_report smb_eval_report(const smb_benchmark_result *result);
 
 #define SMB_BENCHMARK_BEGIN(group_id, description_str, ...)                                                               \
     do {                                                                                                                  \
@@ -48,38 +59,107 @@ static inline unsigned long smb_average_duration_calc_ns(const smb_benchmark_res
             clock_gettime(CLOCK_REALTIME, &time_before);
 
 
-#define SMB_BENCHMARK_END                                                             \
-    clock_gettime(CLOCK_REALTIME, &time_after);                                       \
-    result.timestamps[i] = time_before;                                               \
-    result.timestamps[i + 1] = time_after;                                            \
-    }                                                                                 \
-    if (options.result_storage != NULL) {                                             \
-        *options.result_storage = &result;                                            \
-        continue;                                                                     \
-    }                                                                                 \
-    printf("average execution time %ld ns\n", smb_average_duration_calc_ns(&result)); \
-    free(result.timestamps);                                                          \
-    }                                                                                 \
+#define SMB_BENCHMARK_END                                   \
+    clock_gettime(CLOCK_REALTIME, &time_after);             \
+    result.timestamps[i * 2] = time_before;                 \
+    result.timestamps[i * 2 + 1] = time_after;              \
+    }                                                       \
+    if (options.result_storage != NULL) {                   \
+        *options.result_storage = &result;                  \
+        continue;                                           \
+    }                                                       \
+    smb_benchmark_report report = smb_eval_report(&result); \
+    smb_print_report(&report);                              \
+    free(result.timestamps);                                \
+    }                                                       \
     while (0)
 
 #ifdef SMB_IMPL
+
+#include <math.h>
+#include <stdlib.h>
 
 static const long msins = 1000;
 static const long nsinms = 1000000;
 static const long nsinsec = 1000000000;
 
-static inline unsigned long smb_average_duration_calc_ns(const smb_benchmark_result *result)
+static inline void smb_print_report(const smb_benchmark_report *restrict report)
 {
-    unsigned long sum = 0;
-    for (register size_t i = 0; i < result->runs_count; i++) {
-        sum += smb_timespec_to_timestamp_ns(&result->timestamps[i + 1]) - smb_timespec_to_timestamp_ns(&result->timestamps[i]);
-    }
-    return sum / (unsigned long)result->runs_count;
+    printf("Mean    %40.1f\n", report->average);
+    printf("Min     %40.1f\n", report->min);
+    printf("Max     %40.1f\n", report->max);
+    printf("Median  %40.1f\n", report->median);
+    printf("std dev %40.1f\n", report->std_dev);
 }
 
-static inline long smb_timespec_to_timestamp_ms(struct timespec *time)
+static inline smb_benchmark_report smb_eval_report(const smb_benchmark_result *result)
 {
-    return time->tv_sec * msins + time->tv_nsec / nsinms;
+    double mean = 0;
+    double std_dev = 0;
+    double min = 0;
+    double max = 0;
+    double median = 0;
+    double *durations = malloc(sizeof(*durations) * result->runs_count);
+
+    smb_durations_calc_ns(result, durations);
+    mean = smb_average_duration_calc_ns(durations, result->runs_count);
+    std_dev = smb_std_dev_calc_ns(durations, mean, result->runs_count);
+    smb_sort_durations(durations, result->runs_count);
+    min = durations[0];
+    max = durations[result->runs_count - 1];
+    median = smb_determine_median_ns(durations, result->runs_count);
+    free(durations);
+    return (smb_benchmark_report){
+        .runs_count = result->runs_count, .average = mean, .std_dev = std_dev, .median = median, .min = min, .max = max
+    };
+}
+
+static inline void smb_durations_calc_ns(const smb_benchmark_result *result, double *ret)
+{
+    for (register size_t i = 0; i < result->runs_count; i++) {
+        ret[i] = (double)smb_timespec_to_timestamp_ns(&result->timestamps[i * 2 + 1]) - smb_timespec_to_timestamp_ns(&result->timestamps[i * 2]);
+    }
+}
+
+static inline double smb_average_duration_calc_ns(const double *durations, const size_t count)
+{
+    double sum = 0;
+    for (register size_t i = 0; i < count; i++) {
+        sum += durations[i];
+    }
+    return sum / (double)count;
+}
+
+static inline double smb_std_dev_calc_ns(const double *durations, const double mean, const size_t count)
+{
+    double sum = 0;
+    for (register size_t i = 0; i < count; i++) {
+        sum += pow(durations[i] - mean, 2);
+    }
+    return sqrt(sum / (double)count);
+}
+
+static inline int __smb_sort_compare(const void *a, const void *b)
+{
+    double arg1 = *(const double *)a;
+    double arg2 = *(const double *)b;
+
+    if (arg1 < arg2) return -1;
+    if (arg1 > arg2) return 1;
+    return 0;
+}
+
+static inline void smb_sort_durations(double *durations, const size_t count)
+{
+    qsort(durations, count, sizeof(*durations), __smb_sort_compare);
+}
+
+static inline double smb_determine_median_ns(const double *sorted_durations, const size_t count)
+{
+    if (count % 2 > 0) {
+        return sorted_durations[((count - 1) / 2)];
+    }
+    return (sorted_durations[(count / 2) - 1] + sorted_durations[(count / 2)]) / 2;
 }
 
 static inline unsigned long smb_timespec_to_timestamp_ns(struct timespec *time)
@@ -87,25 +167,6 @@ static inline unsigned long smb_timespec_to_timestamp_ns(struct timespec *time)
     return time->tv_sec * nsinsec + time->tv_nsec;
 }
 
-static inline long smb_timestamp_ms()
-{
-    struct timespec time = smb_time_now();
-    return smb_timespec_to_timestamp_ms(&time);
-}
-
-static inline void smb_abs_timeout(struct timespec *restrict time, long offset_ms)
-{
-    time->tv_nsec += offset_ms * nsinms;
-    time->tv_sec += time->tv_nsec / nsinsec;
-    time->tv_nsec -= (time->tv_nsec / nsinsec) * nsinsec;
-}
-
-static inline struct timespec smb_time_now()
-{
-    struct timespec time;
-    clock_gettime(CLOCK_REALTIME, &time);
-    return time;
-}
 
 #endif// SMB_IMPL
 #endif// SMB_H
--- test/smb-test.c
+++ test/smb-test.c
@@ -6,9 +6,8 @@
 
 STF_TEST_CASE(smb, test)
 {
-    // smb_benchmark_result *res = NULL;
     SMB_BENCHMARK_BEGIN(ass, ass)
-    for (register int i = 0; i < 10000000; i++) {}
+
     SMB_BENCHMARK_END;
 
     STF_EXPECT(true, .failure_msg = "this can't fail");
